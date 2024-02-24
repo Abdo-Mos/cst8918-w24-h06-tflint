d635d687f04832f52b772738db477afbc25e07a9
Abdo-Mos <aabbddoo.sshh@gmail.com>
2024-02-23 23:17:20 -0500

    lab finished

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 # Define output values for later reference
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "name of the resource group description that will be created"
 }
 
 output "vm_name" {
   value = azurerm_linux_virtual_machine.webserver.name
+  description = "name of the vm that will be created"
 }
 
 output "nic_name" {
   value = azurerm_network_interface.webserver.name
+  description = "name of the NIC that will be created"
 }
 
 output "public_ip" {
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
+  description = "name of the public ip that will be created"
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 # Define config variables
-variable "labelPrefix" {
+variable "label_prefix" {
   type        = string
   description = "Your college username. This will form the beginning of various resource names."
 }
 
 variable "region" {
+  type    = string
   default = "westus3"
+  description = "region for the deployment of the resource"
 }
 
 variable "admin_username" {
@@ -13,3 +15,4 @@ variable "admin_username" {
   default     = "azureadmin"
   description = "The username for the local user account on the VM."
 }
+
